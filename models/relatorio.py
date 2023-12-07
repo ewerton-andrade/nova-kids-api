97b0019d09f5b7096efb6d826ba48610fa1a41da
ewerton-andrade <etonandrade@gmail.com>
2023-12-07 11:03:54 -0400

    novos campos no modelo de relatorio

--- models/relatorio.py
+++ models/relatorio.py
@@ -1,8 +1,11 @@
 from db import db
+from datetime import datetime, date
 
 class RelatorioModel(db.Model):
     __tablename__ = "relatorios"
     id = db.Column(db.Integer, primary_key=True)
+    nomeCrianca = db.Column(db.String(100), nullable=False)
+    dataRelatorio = db.Column(db.DateTime, default=datetime.utcnow())
     relatorio= db.Column(db.String, unique=True, nullable=False)
 
     def __repr__(self):
