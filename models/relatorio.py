59f844ca6047528f8418c0087af140e915f0e05d
ewerton-andrade <etonandrade@gmail.com>
2023-12-10 19:41:46 -0400

    refact: changing relatorio schema

--- models/relatorio.py
+++ models/relatorio.py
@@ -1,12 +1,16 @@
 from db import db
 from datetime import datetime, date
+from sqlalchemy.dialects.sqlite import JSON
 
 class RelatorioModel(db.Model):
     __tablename__ = "relatorios"
     id = db.Column(db.Integer, primary_key=True)
     nomeCrianca = db.Column(db.String(100), nullable=False)
     dataRelatorio = db.Column(db.DateTime, default=datetime.utcnow())
-    relatorio= db.Column(db.String, unique=True, nullable=False)
+    relatorio_presenca= db.Column(JSON, nullable=False)
+    relatorio_meditacao= db.Column(JSON, nullable=False)
+    relatorio_versiculo= db.Column(JSON, nullable=False)
+    relatorio_culto= db.Column(JSON, nullable=False)
 
     def __repr__(self):
         return f'<Relatorio {self.id}>'
--- schemas.py
+++ schemas.py
@@ -9,7 +9,13 @@ class UserSchema(Schema):
 
 class RelatorioSchema(Schema):
     id = fields.Str(dump_only=True)
-    relatorio = fields.Str(required=True)
+    nomeCrianca = fields.Str(required=True)
+    dataRelatorio = fields.DateTime(required=True)
+    relatorio_presenca = fields.Dict(keys=fields.Str(), values=fields.Str())
+    relatorio_meditacao = fields.Dict(keys=fields.Str(), values=fields.Str())
+    relatorio_versiculo = fields.Dict(keys=fields.Str(), values=fields.Str())
+    relatorio_culto = fields.Dict(keys=fields.Str(), values=fields.Str())
+
 
 class LogInSchema(Schema):
     username = fields.Str(required=True)
